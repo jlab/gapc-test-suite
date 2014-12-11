4d1899ccb2ec10568796be4531ea268bddb33be0
Stefan Janssen <stefan.m.janssen@gmail.com>
2014-12-11 15:01:05 +0100

    renames Mod_linux to Mod_old, since at CeBiTec cluster barbatus with default gcc, barbatus with gcc-4.8 and zingana all produce the same results as recorded in Truth/Mod. Manual integration of a self compiled mercurial is no longer necessary in run.sh.

--- run.sh
+++ run.sh
@@ -8,15 +8,15 @@
 # $3 number of CPUs used
 
 
-# this needed?
-PATH=/vol/gapc/bin:/vol/gnu/bin:/usr/bin:/vol/pi/bin/GHC/m64/bin:/vol/pi/bin:$PATH
-if [ `uname -s` = "Linux" ]; then
-        export PATH=/vol/pi/share/Installs/Linux/bin:$PATH #for own installations in Linux, whose installers could not be convinced not to install libs into $PREFIX/lib64
-        export PATH=$PATH:/vol/pi/share/Installs/Linux/lib64/python2.6/site-packages/hgext #for mercurial extension under Linux nodes ... 
-else
-        export PATH=$PATH:/vol/mercurial/lib/python2.7/site-packages/hgext #... and architecture with correctly installed mercurial
-fi
-export PATH
+# this needed? Not any longer, since linux nodes at CeBiTec clusters now have mercurial (S.J.)
+#~ PATH=/vol/gapc/bin:/vol/gnu/bin:/usr/bin:/vol/pi/bin/GHC/m64/bin:/vol/pi/bin:$PATH
+#~ if [ `uname -s` = "Linux" ]; then
+        #~ export PATH=/vol/pi/share/Installs/Linux/bin:$PATH #for own installations in Linux, whose installers could not be convinced not to install libs into $PREFIX/lib64
+        #~ export PATH=$PATH:/vol/pi/share/Installs/Linux/lib64/python2.6/site-packages/hgext #for mercurial extension under Linux nodes ... 
+#~ else
+        #~ export PATH=$PATH:/vol/mercurial/lib/python2.7/site-packages/hgext #... and architecture with correctly installed mercurial
+#~ fi
+#~ export PATH
 
 
 # get path to script
